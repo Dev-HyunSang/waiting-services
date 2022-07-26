15e3db8632622ec48ada6cf2cb59d887b81ef319
Dev-HyunSang <me@hyunsang.dev>
2022-07-26 15:40:11 +0900

    feat: update middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,11 +1,35 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"strings"
+
+	"github.com/dev-hyunsang/waiting-services/guest"
+	"github.com/dev-hyunsang/waiting-services/restaurant"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func Middleware(app *fiber.App) {
+	app.Static("/", "./public")
+
+	app.Use(cors.New(cors.Config{
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
+		AllowCredentials: true,
+	}))
+
 	api := app.Group("/api")
 
-	guest := api.Group("/guest")
-	guest.Post("/new", )
-	guest.Get("/list", )
+	Guest := api.Group("/guest")
+	Guest.Post("/new", guest.NewWaitingHandler)
+
+	Restaurant := api.Group("/restaurant")
+	Restaurant.Post("/join", restaurant.RestaurantSignUpHandler)
+	Restaurant.Post("/login", restaurant.RestaurantLogOutHandler)
+	Restaurant.Post("/home", restaurant.RestaurantHomeHandler)
+	Restaurant.Post("/logout", restaurant.RestaurantLogOutHandler)
 }
